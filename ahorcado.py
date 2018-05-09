5660ba63718667ac7c8f79a1541a35fdb1d9bc26
AnnTony1794 <38865402+AnnTony1794@users.noreply.github.com>
2018-05-09 14:04:43 -0500

    Espiral mejorada, agregando de nuevo ahorcado

---
+++ ahorcado.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+
+import random
+import os
+from ctypes import byref, c_int
+
+'''
+Así es como se fedinen los ASCII ART
+'''
+IMAGES = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
+WORDS = [
+    'perder',
+    'telefono',
+    'diccionario',
+    'cantar',
+    'libreta',
+    'acertar',
+    'restringida',
+    'cuarentena'
+]
+
+
+def random_word():
+    index = random.randint(0, len(WORDS) - 1)
+    return WORDS[index]
+
+
+def display_board(hidden_word, tries):
+    print('Juego del ahorcado')
+    print(IMAGES[tries.value])
+    print()
+    print(hidden_word)
+    print('--- *' * len(hidden_word))
+
+
+def game(current_letter, word, tries, hidden_word):
+    if current_letter not in word:
+        if tries.value > 5:
+            print('\nPerdiste')
+            return True
+        tries.value += 1
+    else:
+        for idx, val in enumerate(word):
+            if val == current_letter:
+                hidden_word[idx] = current_letter
+        if '-' not in hidden_word:
+            print('\nLa palabra es: {}'.format(word))
+            print('\n¡Ganaste!')
+            return True
+
+
+def run():
+    word = random_word()
+    hidden_word = ['-'] * len(word)
+    tries = c_int(0)
+    while True:
+        os.system('cls')
+        display_board(hidden_word, tries)
+        current_letter = str(input('Elige una letra:\t'))
+        if len(current_letter) > 1 or len(current_letter) == 0:
+            continue
+
+        # Intento de solución
+
+        if game(current_letter, word, tries, hidden_word):
+            return
+
+    # Termina solución
+
+
+if __name__ == '__main__':
+    run()
--- espiral.py
+++ espiral.py
@@ -7,35 +7,41 @@ def espiral(dimention):
     total_dimention = dimention
     n = 0
 
+    beauty_print = lambda x: str("{}".format(x)) \
+        if x >= 100 \
+        else str(" {}".format(x)
+                 if x in range(10, 100)
+                 else str("  {}".format(x)))
+
     print(counter)
 
     for i in range(0, counter):
 
         for x in range(n, dimention):
-            matrix[i][x] = values
+            matrix[i][x] = beauty_print(values)
             values -= 1
 
         n += 1
 
         for y in range(n, dimention):
-            matrix[y][dimention-1] = values
+            matrix[y][dimention - 1] = beauty_print(values)
             values -= 1
 
         for x in range(n, dimention):
-            matrix[dimention-1][total_dimention - (x + 1)] = values
+            matrix[dimention - 1][total_dimention - (x + 1)] = beauty_print(values)
             values -= 1
 
         n += 1
 
         for x in range(n, dimention):
-            matrix[total_dimention - x][i] = values
+            matrix[total_dimention - x][i] = beauty_print(values)
             values -= 1
 
         dimention -= 1
         n -= 1
 
     for i in range(0, total_dimention):
-        print(matrix[i])
+        print("{}\n".format(matrix[i]))
 
 
 def run():
