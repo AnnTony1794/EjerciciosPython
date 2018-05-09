10296259132705201519cce17954db8cdfb5af2a
AnnTony1794 <38865402+AnnTony1794@users.noreply.github.com>
2018-05-09 10:38:41 -0500

    Refactorizando variables

--- espiral.py
+++ espiral.py
@@ -2,21 +2,14 @@
 
 def espiral(dimention):
     values = (lambda x: x * x)(dimention)
-
+    counter = (lambda val: int((val / 2) + 1) if val % 2 != 0 else int(val / 2))(dimention)
     matrix = [[0 for x in range(dimention)] for y in range(dimention)]
-    counter = 0
-    dim = dimention
     total_dimention = dimention
-    while dim > 0:
-        counter += 1
-        dim -= 2
-
     n = 0
+
     print(counter)
+
     for i in range(0, counter):
-
-
-
 
         for x in range(n, dimention):
             matrix[i][x] = values
@@ -32,12 +25,11 @@ def espiral(dimention):
             matrix[dimention-1][total_dimention - (x + 1)] = values
             values -= 1
 
-        n +=1
+        n += 1
 
         for x in range(n, dimention):
             matrix[total_dimention - x][i] = values
             values -= 1
-
 
         dimention -= 1
         n -= 1
